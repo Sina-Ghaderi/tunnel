446a544649c8be6a8eee4db6346cf05351603196
Sina Ghaderi <sina@snix.ir>
2025-02-22 04:49:12 +0330

    remove errors.go

--- errors.go
+++
@@ -1,8 +0,0 @@
-package tunnel
-
-import "errors"
-
-var (
-	ErrShortPacket      = errors.New("write a short ipv4 or ipv6 packet")
-	ErrFragmentedPacket = errors.New("write a fragmented ipv4 or ipv6 packet")
-)
